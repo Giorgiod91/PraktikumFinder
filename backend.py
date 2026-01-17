9cdcb90ec95dc743c334ca02d5712013ec3b87d6
Giorgiod91 <giorgio.dettmar@gmx.de>
2026-01-17 13:09:22 +0100

    added logic for filtering with plz

--- backend.py
+++ backend.py
@@ -6,10 +6,25 @@ from companies import all_companies
 from fastapi import FastAPI
 
 
+
+# Create a Class and type for the inputs
+
+class User_input:
+    def __init__(self,plz:str, ort:str, difficulty:int):
+        self.input_ort = ort
+        self.input_plz = plz
+        self.input_difficulty = difficulty
+
+
+
+    
+
+
+
 #initalize Fast API
 
 app = FastAPI()
-#@@@@@@
+#@
 # creatine a route to display the companies
 @app.get("/companies")
 # this it a function that displays the companies
@@ -19,6 +34,20 @@ def get_companies():
         companies_to_display.append(i)
     return companies_to_display
 
+
+
+# function to filer for the users given PLZ through an input
+
+# test route for plz if the filter works
+@app.get("/companies/{plz}")
+#
+def filter_plz(plz:str):
+    filtered_companies = []
+    for i in all_companies:
+        if i['plz'] == plz:
+            filtered_companies.append(i)
+
+    return filtered_companies
 
 
 
