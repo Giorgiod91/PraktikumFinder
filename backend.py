afeba8fb74a32aff958fbcc670239ae627425541
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2026-01-17 12:44:59 +0100

    added first route and function to display the companies

--- backend.py
+++ backend.py
@@ -1,8 +1,25 @@
 # for now it will be a harcoded dictionary to showcase the companies where
 # people can apply for a Praktikum for Umschulung since there is no such a site atm
 
-companies ={ 
-        'hard' = ["VW", "Continental"],
-        'normal' = ["startup","Cyber"]
-        }
+from companies import all_companies
+
+from fastapi import FastAPI
+
+
+#initalize Fast API
+
+app = FastAPI()
+#@@@@@@
+# creatine a route to display the companies
+@app.get("/companies")
+# this it a function that displays the companies
+def get_companies():
+    companies_to_display = []
+    for i in all_companies:
+        companies_to_display.append(i)
+    return companies_to_display
+
+
+
+
 
---
+++ companies.py
@@ -0,0 +1,189 @@
+
+# for now it will be a harcoded dictionary to showcase the companies where
+# people can apply for a Praktikum for Umschulung since there is no such a site atm
+
+
+
+all_companies = [
+    {
+        "name": "bundesweit.digital GmbH",
+        "kontakt": "Frau Sophie Peil",
+        "strasse": "Rolandstr. 2-3",
+        "plz": "30161",
+        "ort": "Hannover",
+        "telefon": "051112367321",
+        "email": "info@bundesweit.digital",
+        "homepage": None,
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "CHIPSIZE GmbH",
+        "kontakt": "Avni Korkut",
+        "strasse": "Podbielskistraße 344",
+        "plz": "30655",
+        "ort": "Hannover",
+        "telefon": "0511 – 848 33 – 0",
+        "email": "info@chipsize.com",
+        "homepage": "https://chipsize.com/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "Conix GmbH",
+        "kontakt": "Herr Dirk Rofalski",
+        "strasse": "Lange-Feld-Straße 1",
+        "plz": "30559",
+        "ort": "Hannover",
+        "telefon": "0511 519899-0",
+        "email": "info@conix.de",
+        "homepage": "www.dignes.de",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "Digital cuisine GmbH & Co. KG",
+        "kontakt": "Frau Carina Paulsen",
+        "strasse": "Hanomagstraße 9",
+        "plz": "30449",
+        "ort": "Hannover",
+        "telefon": None,
+        "email": None,
+        "homepage": None,
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "ecoservice Odoo Dienstleister Uwe Böttcher & Falk Neubert GbR",
+        "kontakt": None,
+        "strasse": "Karl-Kellner-Str. 105J",
+        "plz": "30853",
+        "ort": "Langenhagen",
+        "telefon": "+49 (0) 511 515667-40",
+        "email": "info@ecoservice.de",
+        "homepage": "https://www.ecoservice.de/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "GETEC net GmbH",
+        "kontakt": "Herr Marco Achilles",
+        "strasse": "An der Börse 4",
+        "plz": "30159",
+        "ort": "Hannover",
+        "telefon": None,
+        "email": "Marco.Achilles@getec.de",
+        "homepage": None,
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "HIS Hochschul-Informations-System eG",
+        "kontakt": "Herr Thomas Götte",
+        "strasse": "Goseriede 9",
+        "plz": "30159",
+        "ort": "Hannover",
+        "telefon": "+49 (0)511 1220 0",
+        "email": "info@his.de",
+        "homepage": "https://www.his.de/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "IP SYSCON GmbH",
+        "kontakt": "Herr Roland Hachmann",
+        "strasse": "Warmbüchenkamp 4",
+        "plz": "30159",
+        "ort": "Hannover",
+        "telefon": "0511 850303-0",
+        "email": None,
+        "homepage": "https://www.ipsyscon.de/start/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "marketwing GmbH",
+        "kontakt": "Joanna Walaszkowski-Aktas",
+        "strasse": "Steinriede 5a",
+        "plz": "30827",
+        "ort": "Garbsen",
+        "telefon": "05131 45225-0",
+        "email": "jobs@marketwing.de",
+        "homepage": "https://marketwing.de/karriere/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "Pcomplett Frank Bernhardt",
+        "kontakt": "Frau Susanne Bernhardt",
+        "strasse": "Am Listholze 31A",
+        "plz": "30177",
+        "ort": "Hannover",
+        "telefon": "0511 760773-0",
+        "email": "service@pcomplett.de",
+        "homepage": "https://www.pcomplett.de/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "RConsult.biz GmbH",
+        "kontakt": "Herr Daniel Ruther",
+        "strasse": "Raiffeisenstraße 12",
+        "plz": "31275",
+        "ort": "Lehrte",
+        "telefon": "+49 5132 86345-10",
+        "email": "info@rconsult.biz",
+        "homepage": "https://rconsult.biz/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "Ritter Logistik GmbH",
+        "kontakt": "Andreas Ritter",
+        "strasse": "Bayernstraße 46",
+        "plz": "30855",
+        "ort": "Langenhagen",
+        "telefon": "0511 963960",
+        "email": "info.hannover@ritter-logistik.de",
+        "homepage": None,
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "SVLFG (Sozialversicherung für Landwirtschaft, Forsten und Gartenbau)",
+        "kontakt": None,
+        "strasse": "Im Haspelfelde 24",
+        "plz": "30173",
+        "ort": "Hannover",
+        "telefon": None,
+        "email": None,
+        "homepage": None,
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "T4DT GmbH",
+        "kontakt": "Herr Robert Wall",
+        "strasse": "Baumschulenallee 20-22",
+        "plz": "30625",
+        "ort": "Hannover",
+        "telefon": "0511-99987600",
+        "email": "info@t4dt.com",
+        "homepage": "https://t4dt.com/",
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    },
+    {
+        "name": "WITstor GmbH",
+        "kontakt": None,
+        "strasse": "Alfred-Nobel-Str. 2",
+        "plz": "30926",
+        "ort": "Seelze",
+        "telefon": None,
+        "email": None,
+        "homepage": None,
+        "hinweis": "Durch IHK Bestätigt",
+        "category": "normal"
+    }
+]
+
