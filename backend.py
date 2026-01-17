bac4d567e9f2d76803394729608e3cb9ebd45cd8
Giorgio Dettmar <109972616+Giorgiod91@users.noreply.github.com>
2026-01-17 11:47:37 +0100

    initial commit

---
+++ backend.py
@@ -0,0 +1,8 @@
+# for now it will be a harcoded dictionary to showcase the companies where
+# people can apply for a Praktikum for Umschulung since there is no such a site atm
+
+companies ={ 
+        'hard' = ["VW", "Continental"],
+        'normal' = ["startup","Cyber"]
+        }
+
